c1b9b1d049f11dcd114395c61444ccb378968d7b
Federico Allocati <fede.allocati@gmail.com>
2016-03-31 00:02:48 -0300

    Ejercicio 11, funcion accuracy

--- funcional/Tp.hs
+++ funcional/Tp.hs
@@ -23,7 +23,7 @@ split :: Eq a => a -> [a] -> [[a]]
 split x = foldr (\y rec -> if y == x then []:rec else (if null rec then [[y]] else (y:head rec):(tail rec))) []
 
 longitudPromedioPalabras :: Extractor
-longitudPromedioPalabras texto = mean $ map genericLength $ split ' ' texto
+longitudPromedioPalabras texto = mean (map genericLength (split ' ' texto))
 
 apariciones :: Eq a => a -> [a] -> Int
 apariciones x xs = length $ filter (x==) xs
@@ -32,13 +32,13 @@ cuentas :: Eq a => [a] -> [(Int, a)]
 cuentas xs = [(apariciones x xs, x) | x <- nub xs]
 
 repeticionesPromedio :: Extractor
-repeticionesPromedio texto = mean $ map (fromIntegral . fst) $ cuentas $ split ' ' texto
+repeticionesPromedio texto = mean (map (fromIntegral . fst) (cuentas (split ' ' texto)))
 
 tokens :: [Char]
 tokens = "_,)(*;-=>/.{}\"&:+#[]<|%!\'@?~^$` abcdefghijklmnopqrstuvwxyz0123456789"
 
 frecuenciaTokens :: [Extractor]
-frecuenciaTokens = [ \xs -> (fromIntegral $ apariciones token xs) / (fromIntegral $ length xs) | token <- tokens]
+frecuenciaTokens = [ \xs -> (fromIntegral (apariciones token xs)) / (fromIntegral (length xs)) | token <- tokens]
 
 normalizarExtractor :: [Texto] -> Extractor -> Extractor
 normalizarExtractor = undefined
@@ -47,22 +47,31 @@ extraerFeatures :: [Extractor] -> [Texto] -> Datos
 extraerFeatures extractores textos = map (\texto -> map (\extractor -> (normalizarExtractor textos extractor) texto) extractores) textos
 
 distEuclideana :: Medida
-distEuclideana p q = sqrt $ sum $ map (**2) $ zipWith (-) p q
+distEuclideana p q = sqrt (sum (map (**2) (zipWith (-) p q)))
 
 prodVectorial :: [Float] -> [Float] -> Float
-prodVectorial p q = sum $ zipWith (*) p q
+prodVectorial p q = sum (zipWith (*) p q)
 
 normaVectorial :: [Float] -> Float
-normaVectorial p = sqrt $ prodVectorial p p
+normaVectorial p = sqrt (prodVectorial p p)
 
 distCoseno :: Medida
 distCoseno p q = (prodVectorial p q) / (normaVectorial p * normaVectorial q)
 
+distanciasAInstancia :: Datos -> Medida -> Instancia -> [Float]
+distanciasAInstancia datos medida instancia = map (medida instancia) datos
+
+modaEstadistica :: Eq a => [a] -> a
+modaEstadistica xs = snd (foldr (\x rec -> if fst x > fst rec then x else rec) (0, head xs) (cuentas xs))
+
 knn :: Int -> Datos -> [Etiqueta] -> Medida -> Modelo
-knn = undefined
+knn k datos etiquetas medida = \instancia -> modaEstadistica (map snd (take k (sort (zip (distanciasAInstancia datos medida instancia) etiquetas))))
 
 accuracy :: [Etiqueta] -> [Etiqueta] -> Float
-accuracy = undefined
+accuracy predicciones etiquetas= sum (map boolAFloat (zipWith (==)  predicciones etiquetas)) / (genericLength predicciones)
+        where boolAFloat b
+                    | b == True = 1.0
+                    | otherwise = 0.0
 
 separarDatos :: Datos -> [Etiqueta] -> Int -> Int -> (Datos, Datos, [Etiqueta], [Etiqueta])
 separarDatos = undefined
