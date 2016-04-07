1b592ae16a2ddd589dc1dac45a5e172fcbeefd45
Sacha Sebastian Kantor <skantor@ws9.labo6>
2016-04-07 16:48:34 -0300

    Comentarios agregados para los ejercicios del 1-8 inclusive. Pequeña modificación para eliminar un if del código del ejercicio 1 (split)

--- funcional/Tp.hs
+++ funcional/Tp.hs
@@ -22,41 +22,97 @@ tryClassifier x y = let
 mean :: [Float] -> Float
 mean xs = realToFrac (sum xs) / genericLength xs
 
-split :: Eq a => a -> [a] -> [[a]]
-split x xs = filter (not . null) (foldr (\y rec -> if y == x then []:rec else (if null rec then [[y]] else (y:head rec):(tail rec))) [] xs)
+{- split: Consultamos caracter a caracter (o elemento a elemento de la lista, en realidad)
+si el mismo es el elemento separador. Si no lo es lo agregamos a la primera cadena/lista
+del resultado (que es una lista de listas), caso contrario insertamos una nueva cadena/lista
+vacia al comienzo del resultado.
 
+Por ultimo, filtramos las posibles cadenas/listas vacias que tenga el resultado (esto sería
+el paso 'trim' del que se habló en clase.
+-}
+split :: Eq a => a -> [a] -> [[a]]
+split x xs = filter (not . null) (foldr (\y rec -> if y == x then []:rec else (y:head rec):(tail rec)) [[]] xs)
+
+
+{- longitudPromedioPalabras: Convierte el parametro de entrada (de tipo Texto) en una lista
+de palabras, y luego convierte cada palabra a un número representando la longitud de la misma.
+Por último, calcula el promedio de todas estas longitudes.
+
+Ej: "Hola, ¿Cómo te va? -> ["Hola,","¿Cómo","te","va?"] -> [5,5,2,3] -> 3.75
+-}
 longitudPromedioPalabras :: Extractor
 longitudPromedioPalabras texto = mean (map genericLength (split ' ' texto))
 
+
+{- cuentas: Se toma cada elemento de la lista original (sin tomar en cuenta los repetidos,
+y se cuenta la cantidad de apariciones que tiene en la lista original. Luego "agrega" la
+tupla <#aparciones(elemento),elemento> al resultado final) 
+-}
 apariciones :: Eq a => a -> [a] -> Int
 apariciones x xs = length $ filter (x==) xs
 
 cuentas :: Eq a => [a] -> [(Int, a)]
 cuentas xs = [(apariciones x xs, x) | x <- nub xs]
 
+
+{- repeticionesPromedio: Convertimos el texto de entrada a una lista de palabras (con
+split), luego convertimos esta en una lista de tuplas <#aparciones,elemento> para por
+ultimo calcular el promedio de las componentes izquierdas (convertidas a Float) de
+todas estas tuplas.
+
+Ej: "Y dale, dale, dale dale, dale tense" 
+	-> [[Y],[dale,],[dale,],[dale],[dale,],[dale],[tense]]
+	-> [(1,"Y"),(3,"dale,"),(2,"dale"),(1,"tense")]
+	-> [1.0,3.0,2.0,1.0]
+	-> 1.75
+-}
 repeticionesPromedio :: Extractor
 repeticionesPromedio texto = mean (map (fromIntegral . fst) (cuentas (split ' ' texto)))
 
+
+{- frecuenciasTokens: Crea una lista por comprensión de funciones (expresadas como
+funciones lambda). Cada una de estas funciones tienen un token fijo, y al pasarles
+un texto calculan la frecuencia relativa de dicho token respecto de la longitud
+del texto.
+-}
 tokens :: [Char]
 tokens = "_,)(*;-=>/.{}\"&:+#[]<|%!\'@?~^$` abcdefghijklmnopqrstuvwxyz0123456789"
 
 frecuenciaTokens :: [Extractor]
 frecuenciaTokens = [ \texto -> (fromIntegral (apariciones token texto)) / (fromIntegral (length texto)) | token <- tokens]
 
+
+{- normalizarExtractor: Básicamente, tomamos el resultado del extractor para cada
+texto (feature) y normalizamos a cada uno de ellos dividiendolo por el de mayor
+valor absoluto (el màximo feature en módulo). Es decir, normalizamos los features
+de un extractor para una lista de textos relativo al feature de mayor valor
+absoluto.
+-}
 normalizarExtractor :: [Texto] -> Extractor -> Extractor
 normalizarExtractor textos extractor = let
-                                                                    features = map extractor textos        
-                                                                    escala = foldl (\rec x -> max rec (abs x)) 0 features
-                                                                    in (\texto -> (extractor texto) / (if escala > 0 then escala else 1))
+                                                                    features = map extractor textos -- Renombre para el feature obtenido con el extractor para cada texto
+                                                                    escala = foldl (\rec x -> max rec (abs x)) 0 features --Calculamos el maximo de los features
+                                                                    in (\texto -> (extractor texto) / (if escala > 0 then escala else 1)) --Funcion devuelta/definida
 
+
+{- extraerFeatures: Primero "transformamos" (abuso de léxico, nada se "transforma"
+en funcional) los extractores en extractores normalizados relativos a los textos.
+Luego aplicamos estos extractores normalizados a cada uno de los textos.
+-}
 extraerFeatures :: [Extractor] -> [Texto] -> Datos
 extraerFeatures extractores textos = let
                                                                 extractoresNorm = map (\extractor -> normalizarExtractor textos extractor) extractores
                                                                 in  map (\texto -> map (\extractor -> extractor texto) extractoresNorm) textos
 
+
+{- distEuclideana: Trivial (muy). No creemos que requiera aclaraciones.
+-}
 distEuclideana :: Medida
 distEuclideana p q = sqrt (sum (map (**2) (zipWith (-) p q)))
 
+
+{- distCoseno: Idem distEuclideana
+-}
 prodVectorial :: [Float] -> [Float] -> Float
 prodVectorial p q = sum (zipWith (*) p q)
 
@@ -65,6 +121,7 @@ normaVectorial p = sqrt (prodVectorial p p)
 
 distCoseno :: Medida
 distCoseno p q = (prodVectorial p q) / (normaVectorial p * normaVectorial q)
+
 
 distanciasAInstancia :: Datos -> Medida -> Instancia -> [Float]
 distanciasAInstancia datos medida instancia = map (medida instancia) datos
