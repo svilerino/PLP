80e02b20a4c4afc989b5103d4fda1cbb358f827a
Sacha Kantor <sacha.kantor@pjn.gov.ar>
2016-04-12 12:20:10 -0300

    Achurado y pulido de comentarios de funciones

--- funcional/Tp.hs
+++ funcional/Tp.hs
@@ -21,24 +21,28 @@ mean :: [Float] -> Float
 mean xs = realToFrac (sum xs) / genericLength xs
 
 --------------------------------------------------------------------------------
-{- split: Consultamos caracter a caracter (o elemento a elemento de la lista, en realidad)
-si el mismo es el elemento separador. Si no lo es lo agregamos a la primera cadena/lista
-del resultado (que es una lista de listas), caso contrario insertamos una nueva cadena/lista
-vacia al comienzo del resultado.
+{- split: La función pasada a foldr lo que hace es agregar caracteres (mientras
+no sea un caracter separador) al primer elemento de la lista de resultado (un
+string). En el caso base se agrega un string vacío dentro de la lista resultado,
+dado que en caso de que el primer caracter leido sea un separador, la lista/string
+vacío resultante sera eliminado del resultado final mediante el filter (ya que
+como se explico en clase, había que hacer un 'trim' de los resultados).
 
-Por ultimo, filtramos las posibles cadenas/listas vacias que tenga el resultado (esto sería
-el paso 'trim' del que se habló en clase.
+Ej: split ',' ",Saraza,,Saraza,"
+	-> [[],"Saraza",[],"Saraza",[],[]]
+	-> ["Saraza","Saraza"]
 -}
 split :: Eq a => a -> [a] -> [[a]]
 split x xs = filter (not . null) (foldr (\y rec -> if y == x then []:rec else (y:head rec):(tail rec)) [[]] xs)
 
 
 --------------------------------------------------------------------------------
-{- longitudPromedioPalabras: Convierte el parametro de entrada (de tipo Texto) en una lista
-de palabras, y luego convierte cada palabra a un número representando la longitud de la misma.
-Por último, calcula el promedio de todas estas longitudes.
+{- longitudPromedioPalabras: No requiere explicación
 
-Ej: "Hola, ¿Cómo te va? -> ["Hola,","¿Cómo","te","va?"] -> [5,5,2,3] -> 3.75
+Ej: longitudPromedioPalabras "Hola, ¿Cómo te va?
+	-> ["Hola,","¿Cómo","te","va?"]
+	-> [5,5,2,3]
+	-> 3.75
 -}
 longitudPromedioPalabras :: Extractor
 longitudPromedioPalabras texto = case texto of
@@ -47,9 +51,7 @@ longitudPromedioPalabras texto = case texto of
 
 
 --------------------------------------------------------------------------------
-{- cuentas: Se toma cada elemento de la lista original (sin tomar en cuenta los repetidos,
-y se cuenta la cantidad de apariciones que tiene en la lista original. Luego "agrega" la
-tupla <#aparciones(elemento),elemento> al resultado final) 
+{- cuentas: No Requiere explicación
 -}
 apariciones :: Eq a => a -> [a] -> Int
 apariciones x xs = length $ filter (x==) xs
@@ -59,10 +61,7 @@ cuentas xs = [(apariciones x xs, x) | x <- nub xs]
 
 
 --------------------------------------------------------------------------------
-{- repeticionesPromedio: Convertimos el texto de entrada a una lista de palabras (con
-split), luego convertimos esta en una lista de tuplas <#aparciones,elemento> para por
-ultimo calcular el promedio de las componentes izquierdas (convertidas a Float) de
-todas estas tuplas.
+{- repeticionesPromedio: No requiere explicación.
 
 Ej: "Y dale, dale, dale dale, dale tense" 
 	-> [[Y],[dale,],[dale,],[dale],[dale,],[dale],[tense]]
@@ -81,6 +80,12 @@ repeticionesPromedio texto = case texto of
 funciones lambda). Cada una de estas funciones tienen un token fijo, y al pasarles
 un texto calculan la frecuencia relativa de dicho token respecto de la longitud
 del texto.
+
+Se tomó en cuenta el caso en que si el texto que se le pasa a cada función de
+frecuenciasTokens es nulo, entonces las funciones devuelvan 0 (tiene sentido
+pensar que la frecuencia de cualquier token en un string vacio sea 0, que sería
+idéntica a pasarle un texto donde dicho token no aparezca).
+
 -}
 tokens :: [Char]
 tokens = "_,)(*;-=>/.{}\"&:+#[]<|%!\'@?~^$` abcdefghijklmnopqrstuvwxyz0123456789"
@@ -97,6 +102,11 @@ texto (feature) y normalizamos a cada uno de ellos dividiendolo por el de mayor
 valor absoluto (el màximo feature en módulo). Es decir, normalizamos los features
 de un extractor para una lista de textos relativo al feature de mayor valor
 absoluto.
+
+La decisión de usar "in" en lugar de "where" se consultó por mail, y se decidió
+usar "in" por cuestiones de performance de la implementación de ghc.
+
+No se muestra un ejemplo, ya que el resultado es un extractor.
 -}
 normalizarExtractor :: [Texto] -> Extractor -> Extractor
 normalizarExtractor textos extractor = let
@@ -106,24 +116,40 @@ normalizarExtractor textos extractor = let
 
 
 --------------------------------------------------------------------------------
-{- extraerFeatures: Primero "transformamos" (abuso de léxico, nada se "transforma"
-en funcional) los extractores en extractores normalizados relativos a los textos.
-Luego aplicamos estos extractores normalizados a cada uno de los textos.
+{- extraerFeatures: No requiere explicación.
+
+No se muestra ejemplo, ya que los pasos intermedios representarian funciones,
+las cuales no sabríamos como explicitar de forma escrita.
 -}
 extraerFeatures :: [Extractor] -> [Texto] -> Datos
 extraerFeatures extractores textos = let
     extractoresNorm = map (\extractor -> normalizarExtractor textos extractor) extractores
-    in  map (\texto -> map (\extractor -> extractor texto) extractoresNorm) textos
+    in  map (\texto -> map (\extractorNormalizado -> extractorNormalizado texto) extractoresNorm) textos
 
 
 --------------------------------------------------------------------------------
---distEuclideana: Trivial (muy). No creemos que requiera aclaraciones.
+{-distEuclideana: No requiere explicación.
+
+Ej: distEuclideana [4,4] [2,2]
+	-> [2,2]
+	-> [4,4]
+	-> 8
+	-> 2.828427
+-}
 distEuclideana :: Medida
 distEuclideana p q = sqrt (sum (map (**2) (zipWith (-) p q)))
 
 
 --------------------------------------------------------------------------------
---distCoseno: Idem distEuclideana
+{-distCoseno: No requiere explicación.
+
+Ej: distCoseno [4,4] [2,2] --Los ejemplos no muestran el orden de evaluación de Haskell
+	-> sum [8,8] / (sqrt(sum [8,8]) * sqrt(sum [4,4]))
+	-> 16 / (sqrt(16) * sqrt(8))
+	-> 16 / (4*2.82)
+	-> 1.4184
+
+-}
 prodVectorial :: [Float] -> [Float] -> Float
 prodVectorial p q = sum (zipWith (*) p q)
 
@@ -135,36 +161,25 @@ distCoseno p q = (prodVectorial p q) / (normaVectorial p * normaVectorial q)
 
 
 --------------------------------------------------------------------------------
-{- knn: La función knn se apocha en dos funciones auxiliares, distanciasAInstancia
-y modaEstadística. Primero comentaremos como funcionan estas funciones auxiliares
-y luego pasaremos a explicar knn en si mismo.
+{- knn: Consideramos que la implementación de esta función es bastante legible
+y no requiere de demasiados comentarios que lo expliquen. De lo que por ahí
+requiere alguna aclaración es el uso del "if" en la función modaEstadística
+y el uso de sort en la definición misma de knn.
 
-La primera, distanciasAInstancia, simplemente calcula la distancia de la
-instancia de entrada a todas las instancias de los Datos (es decir, a cada
-elemento de Datos). Esta distancia está definida por el parametro medida (que
-es una funcion de 2 instancias en un float). Básicamente tomtamos la función
-medida y se aplica a todos los elementos de Datos contra la instancia de
-entrada.
+Sobre lo primero, surge de considerar el caso en que el conjunto de etiquetas
+sea vacío (esto puede ocurrir si se invoca a knn sin Datos ni Etiquetas -las
+que les pasa nFoldCrossValidation como datos de entrenamiento-, aunque no tiene
+mucho sentido tratar de usar este "programa" así). Entonces, consideramos
+lógico que knn devolviese siempre una etiqueta default (en este caso, "i")
+si no llegase a ser invocado con datos. Se podría haber usado una devolución
+aleatoria de entre las etiquetas posibles como resultado (sería etiquetar de
+manera random), pero lo desestimamos ya que al no ser deterministico no se
+podría "testear".
 
-La segunda, modaEstadística, simplemente toma una lista de elementos y nos
-devuelve el elemento de la misma que más apariciones tiene. Para ello, primero
-calcula la cantidad de repeticiones de cada elemento (generando una lista de
-tuplas <#aparciones,elemento> mediante la función cuentas) para luego utilizar
-la extructura foldr para buscar la tupla cuya primer componente (la cantidad
-de repeticiones) es la más grande. Por último, nos quedamos sólo con el
-elemento (es decir, con la segunda componente de esta tupla).
-
-Ahora sí, knn lo que hace es obtener una lista de distancias de las instancias
-de entrenamientos a la instancia de entrada (utilizando la Medida pasada por
-parámetro) y luego asociar cada elemento de esta lista (que representa una
-distancia) con la etiqueta de la instancia de entrenamiento correspondiente
-(esta asociación se realiza mediante zip, conviertiendo los resultados en
-una lista de tuplas <distancia,etiqueta>). Luego, esta lista se ordena de
-menor a mayor mediante la primer componente de las tuplas y se toman los primeros
-k (primer parámentro) elementos (es decir, los k vecinos a menor distancia).
-Luego se convierte esta lista de tuplas en una lista de únicamente las segundas
-componentes de las mismas (es decir, con las etiquetas) para luego quedarnos con
-aquella que se repita más (mediante modaEstadistica).
+Sobre lo segundo, la única aclaración es que sort ordena de menor a mayor,
+y en caso de ordenar tuplas, las ordena primero según su primer componente,
+y en caso de empate desempata según el orden de la componente siguiente
+(de menor a mayor también).
 
 Ej: (knn 2 [[1,10],[1,5],[5,1]] [f,i,i] distEuclideana) [1,1]
     -> [9.0,4.0,4.0]
@@ -185,26 +200,7 @@ knn k datos etiquetas medida = \instancia -> modaEstadistica (map snd (take k (s
 
 
 --------------------------------------------------------------------------------
-{- separarDatos: Esta función es simplemente la aplicación para distintas listas
-(la de datos y la de etiquetas) de dos funciones: obtenerSalvoParticion y
-obtenerParticion.
-
-Básicamente lo que hacen es particionar una lista de entrada en  n particiones
-y devolver la partición p (parámentro), en el caso de obtenerParticion, o 
-devolver toda la lista original excepto la partición p (obtenerSalvoParticion).
-
-obtenerPartición lo que hace es calcular el tamaño de cada partición dada la
-lista original (y su cantidad de elementos) y el tamaño de la partición pasado
-por parámetro, para luego eliminar los elementos de las primeras p-1 particiones
-y luego tomar de las elementos restantes los primeros longParticion elementos 
-(para p la partición a obtener y longParticion la longitud de una particion).
-
-obtenerSalvoPartición es muy similar a obtenerParticion. Calcula el tamaño de la
-partición en base al tamaño de la lista y la cantidad de particiones, y luego
-calcula la cantidad de elementos antes y después de la particón a ser "filtrada".
-Por último, simplemente tomamos los elementos de todas las particiones previas
-a la p (la que va a ser filtrada) y se concatena con todos los elementos post
-partición p.
+{- separarDatos: No requiere explicación.
 -}
 obtenerParticion :: Int -> Int -> [a] -> [a]
 obtenerParticion n p xs = let
@@ -218,19 +214,23 @@ obtenerSalvoParticion n p xs = let
     longPrimerParte = (p - 1) * longParticion
     longUltimaParte = (n - p) * longParticion
     longHastaUltimaParte = longPrimerParte + longParticion
-    in take longPrimerParte xs ++ take longUltimaParte (drop longHastaUltimaParte xs) --se podría hacer con reverse reverse y evitar un calculo, aunque reverse no es muy performante... también se podría con un dropWhile
+    in take longPrimerParte xs ++ take longUltimaParte (drop longHastaUltimaParte xs) {-se podría hacer con reverse reverse 
+											y evitar un calculo, aunque reverse 
+											no funciona rápido, especialmente con
+											listas largas-}
 
 separarDatos :: Datos -> [Etiqueta] -> Int -> Int -> (Datos, Datos, [Etiqueta], [Etiqueta])
 separarDatos datos etiquetas n p = (obtenerSalvoParticion n p datos, obtenerParticion n p datos, obtenerSalvoParticion n p etiquetas, obtenerParticion n p etiquetas)
 
 
 --------------------------------------------------------------------------------
-{- accuracy: Simplemente tomamos ambas listas, y generamos una lista de boolean
-resultado de comparar elemento a elemento (en la misma posicón) las 2 listas
-de entrada. Luego convertimos todos los valores de esta lista True a 1 y los
-False a 0. Sumamos, obteniendo la cantidad de aciertos en las predicciones.
-Luego y dividimos por la longitud de las listas, es decir, la cantidad de
-predicciones, los cual nos termina dando un porcentaje de acierto en lo predicho.
+{- accuracy: No requiere explicación
+
+Ej: accuracy ["i","f","f","i","i","f"] ["i","i","i","f","f","f"]
+	-> [True,False,False,False,False,True]
+	-> [1,0,0,0,0,1]
+	-> 2 / 6
+	-> 0.333333
 -}
 boolAFloat :: Bool -> Float
 boolAFloat b = case b of
@@ -242,15 +242,8 @@ accuracy predicciones etiquetas = sum (map boolAFloat (zipWith (==)  prediccione
 
 
 --------------------------------------------------------------------------------
-{- nFoldCrossValidation: Esta función termina siendo el promedio de una lista
-definida por comprensión, donde cada uno de sus elementos es la precisión
-(accuracy) de utilizar una partición distinta como conjunto de validación, y
-todas las demás como particiones de entrenamiento. Entonces, cada elemento
-de esta lista se calcula primero separando los datos en las particiones
-(mediante separarDatos) para luego aplicar mediante map el modelo vecinos
-más cercanos con K=15 a cada instancia de la partición de validación y luego
-calcular el accuracy de dichas aplicaciones.
+{- nFoldCrossValidation: No requiere explicación.
 -}
 nFoldCrossValidation :: Int -> Datos -> [Etiqueta] -> Float
-nFoldCrossValidation n datos etiquetas = mean [ obtenerAccuracy (separarDatos datos etiquetas n fold) | fold <- [1..n] ]
+nFoldCrossValidation n datos etiquetas = mean [ obtenerAccuracy (separarDatos datos etiquetas n particion_a_validar) | particion_a_validar <- [1..n] ]
     where obtenerAccuracy (datos_train, datos_val, etiquetas_train, etiquetas_val) = accuracy (map (knn 15 datos_train etiquetas_train distEuclideana) datos_val) etiquetas_val
