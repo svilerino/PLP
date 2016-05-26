4bbf9ca97a77e2f5158f80c7585d209a1653ae31
Sacha Kantor <sacha.kantor@pjn.gov.ar>
2016-05-26 08:28:07 -0300

    Agregado de los archivos iniciales del tp2 (prolog)

---
+++ prolog/esqueleto.pl
@@ -0,0 +1,28 @@
+:- dynamic(diccionario/1).
+
+% Dado un nombre de archivo que contiene todas las palabras que se quieren
+% agregar al diccionario (una por linea), vacia diccionario/1 y agrega
+% las definiciones nuevas
+
+cargar(NombreDeArchivo) :-
+  retractall(diccionario(_)),
+  atom_codes(NombreDeArchivo, Arch),
+  open(Arch, read, Str),
+  read_file(Str,_),
+  close(Str).
+
+read_file(Stream,[]) :- at_end_of_stream(Stream).
+read_file(Stream,[X|L]) :-
+    not(at_end_of_stream(Stream)),
+    read_line_to_codes(Stream,Codes),
+    string_codes(X, Codes),
+    assertz(diccionario(X)),
+    read_file(Stream,L), !.
+
+
+% listar mensajes secretos de ejemplo.
+ej(1, [rombo, cuadrado, espacio, perro, cuadrado, sol, cuadrado]).
+% solo debería ser "la cosa" porque cuadrado != triangulo
+ej(2, [rombo, cuadrado, espacio, perro, triangulo, sol, cuadrado]).
+
+ej(3, [rombo, cuadrado, perro, cuadrado, sol, luna, triangulo, estrella, arbol, gato]).
