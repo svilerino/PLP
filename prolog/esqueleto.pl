7aa5136e573aedf88ede0e8e4e044f1d51ba5bd5
Federico Allocati <fede.allocati@gmail.com>
2016-06-01 15:55:34 -0300

    Ejercicio 2

--- prolog/esqueleto.pl
+++ prolog/esqueleto.pl
@@ -20,6 +20,7 @@ read_file(Stream,[X|L]) :-
     read_file(Stream,L), !.
 
 
+
 % listar mensajes secretos de ejemplo.
 ej(1, [rombo, cuadrado, espacio, perro, cuadrado, sol, cuadrado]).
 % solo debería ser "la cosa" porque cuadrado != triangulo
@@ -33,3 +34,15 @@ ej(3, [rombo, cuadrado, perro, cuadrado, sol, luna, triangulo, estrella, arbol, 
 % el resultado del predicado es true si el string que representa pertenece al 
 % diccionario actual.
 diccionario_lista(Y) :- diccionario(X), string_codes(X, Y).
+
+% juntar_con(+L, ?J, ?R)
+% L debe estar instanciado siempre, ya que sino se cuelga. Si J esta instanciado y R no,
+% R se va a instanciar con la union de L intercalado con J. Si R esta instanciado y J no,
+% J se instancia con el unico elemento posible (si existe) que haga que R sea el resultado,
+% de intercalar este elemento entre los elementos de L. Si ni J ni R estan instanciados,
+% J queda sin instanciar, y R se instancia con la lista intercalada de L, teniendo como
+% elemento de intercalacion la variable que se haya elegido como J.
+juntar_con([], _, []).
+juntar_con([X], _, X).
+juntar_con([X | Xs], J, R) :- length(Xs, Len), Len > 0, append(X, [J], Left), juntar_con(Xs, J, Right), append(Left, Right, R).
+
